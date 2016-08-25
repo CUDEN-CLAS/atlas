ce788f61a24f26850b56c4ea9c45ead1765f57f4
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-08-25 14:54:29 -0600

    More mocking.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['python_ldap', 'ldap']
+MOCK_MODULES = ['python-ldap', 'ldap']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
