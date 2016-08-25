57b4a11269fe78bc354d063a5c15891375e3ab50
James W Fuller <James.W.Fuller@Colorado.EDU>
2016-08-25 13:36:31 -0600

    Correct naming.

--- docs/conf.py
+++ docs/conf.py
@@ -345,5 +345,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['python-ldap']
+MOCK_MODULES = ['python_ldap']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
