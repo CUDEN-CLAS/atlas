8ab892ab3cef644fb3683d4848a5d779a03d31c4
stalkert <Tim.Stalker@ucdenver.edu>
2017-01-13 16:32:27 -0700

    alternative rr command

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -606,7 +606,7 @@ def _push_settings_files(site, directory):
 def _install_site(profile_name, code_directory_current):
     with cd(code_directory_current):
         run('drush site-install -y {0}'.format(profile_name))
-        run('drush rr')
+        run('drush php-eval "registry_rebuild();"')
 
 
 def _clone_repo(git_url, checkout_item, destination):
