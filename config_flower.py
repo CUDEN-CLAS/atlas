5f0965f50e513df9693eb6b6325adca3310aa561
stalkert <Tim.Stalker@ucdenver.edu>
2017-06-29 14:22:52 -0600

    updated flower settings for prod

--- config_flower.py
+++ config_flower.py
@@ -1,7 +1,7 @@
 # RabbitMQ management api
 broker_api = 'amqp://guest@localhost:5672/api/'
 
-address = 'clas-invtest.ucdenver.pvt'
+address = 'clas-invprod.ucdenver.edu'
 
 # Enable debug logging
 logging = 'DEBUG'
