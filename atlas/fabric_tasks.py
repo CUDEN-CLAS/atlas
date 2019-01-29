c7a886c35bf4065e193262e0e22d23bf5fbe7e0c
Tim Stalker <Tim.Stalker@ucdenver.edu>
2019-01-29 14:33:36 -0700

    dslm package name instead of name+version

--- atlas/fabric_tasks.py
+++ atlas/fabric_tasks.py
@@ -242,7 +242,7 @@ def site_package_update(site):
     package_name_string = ""
     for package in site['code']['package']:
         # Append the package name and a space.
-        package_name_string += utilities.get_code_name_version(package) + " "
+        package_name_string += utilities.get_code_name(package) + " "
     # Strip the trailing space off the end.
     package_name_string = package_name_string.rstrip()
 
